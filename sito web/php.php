94855b5590af99a91dc3e7b48ccb841b727c7b41
Haelusus <graziadevivo13@gmail.com>
2025-11-28 12:52:30 +0100

    provato a fare un sito

---
+++ sito web/php.php
@@ -0,0 +1,14 @@
+<?php
+$rand = rand(1,10);
+
+if($button){
+    echo "Hai premuto il bottone!";
+}
+for($i=1;$i<$rand;$i++){
+    return $card1;
+    count 1;
+    if($num==17){
+        echo "sembra che tu abbia avuto sfortuna!";
+    }
+}
+?>
